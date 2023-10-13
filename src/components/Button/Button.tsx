5e75369684e0fa1a3067bf51655ff4abd576bda9
keithburgie <krburgie@gmail.com>
2023-10-13 09:49:47 -0400

    update palets package, play with colors

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ButtonHTMLAttributes } from "react";
 import classnames from "classnames";
-import { semanticColorKeys, shadeRange } from "../../../theme";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   // colorScheme: string;
 }
 
 const Button = ({ ...props }: ButtonProps) => {
-  // const errolsButton =
-  //   "mt-4 py-4 px-8 text-xl font-bold rounded-3xl lg:py-4 focus:outline-none shadow-xl";
+  const errolsButton =
+    "mt-4 py-4 px-8 text-xl font-bold rounded-3xl lg:py-4 focus:outline-none shadow-xl";
+
   // const errolsButton = {
   //   "mt-4": true,
   //   "py-4": true,
@@ -21,16 +22,16 @@ const Button = ({ ...props }: ButtonProps) => {
   //   "shadow-xl": true,
   // };
 
-  // const allyDefauly =
-  //   "px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 active:bg-blue-800 disabled:opacity-50";
+  const allyDefauly =
+    "px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 active:bg-blue-800 disabled:opacity-50";
 
-  // const htmlButton =
-  //   "px-3 py-2 bg-primary-400 text-black border rounded-md border-gray-300 focus:border-blue-500 focus:outline-none focus:shadow-outline hover:bg-gray-200 active:bg-gray-300";
+  const htmlButton =
+    "px-3 py-2 bg-primary-400 text-black border rounded-md border-gray-300 focus:border-blue-500 focus:outline-none focus:shadow-outline hover:bg-gray-200 active:bg-gray-300";
 
   const htmlButtonClassNames: Record<string, boolean> = {
     "px-3": true,
     "py-2": true,
-    "text-black": true,
+    "text-black": true, // this is being made pink (on purpose!) by tailwind.config.js
     border: true,
     "rounded-md": true,
     "focus:border-blue-500": true,
@@ -44,7 +45,7 @@ const Button = ({ ...props }: ButtonProps) => {
 
   // htmlButtonClassNames[`bg-${colorScheme}-100`] = true;
   // htmlButtonClassNames[`border-${colorScheme}-300`] = true;
-  // htmlButtonClassNames[`hover:bg-${colorScheme}-200`] = true;
+  // htmlButtonClassNames[`hover:bg-${colorScheme}-200`] = true;`
   // htmlButtonClassNames[`active:bg-${colorScheme}-300`] = true;
 
   const accessibleHtmlButton = classnames({
@@ -57,19 +58,26 @@ const Button = ({ ...props }: ButtonProps) => {
 
   return (
     <>
-      <button className={`${accessibleHtmlButton} mb-4`} {...props}>
-        Button
+      <button
+        className={`${accessibleHtmlButton} mb-4 bg-black text-white`}
+        {...props}
+      >
+        Black Button
       </button>
 
-      <div className="grid grid-cols-11">
-        {semanticColorKeys.map((color) =>
-          shadeRange.map((shade) => (
-            <button key={color + shade} className={`bg-${color}-${shade}`}>
-              {shade}
-            </button>
-          ))
-        )}
-      </div>
+      <button
+        className={`${accessibleHtmlButton} mb-4 bg-red-900 hover:bg-red-700 text-white`}
+        {...props}
+      >
+        Red Button
+      </button>
+
+      <button
+        className={`${accessibleHtmlButton} mb-4 bg-blue-900 hover:bg-blue-700 text-white hover:text-black`}
+        {...props}
+      >
+        Blue Button
+      </button>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,33 @@
 // From https://tailwindcss.com/docs/guides/vite
+import { createColorPalettes } from "palets";
 
-import semanticColors from "./theme";
+const colorsFromPalets = createColorPalettes([
+  {
+    colorName: "black",
+    // hex for pink: #f87171
+    singleValue: " #f87171", // <-- whatever you put here will display when "black" is called
+  },
+  { colorName: "white", singleValue: "#ffffff" },
+  {
+    colorName: "blue",
+    primaryValue: "#60a5fa",
+    type: "alpha",
+  },
+  {
+    colorName: "red",
+    // hex for green: #34d399
+    primaryValue: "#34d399",
+  },
+]);
+
+console.log(colorsFromPalets);
 
 /** @type {import('tailwindcss').Config}*/
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: semanticColors,
+      colors: colorsFromPalets,
     },
   },
   plugins: [],
